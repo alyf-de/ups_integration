b54f7a31723f701f915cdbd9d3ee30ac6e73d293
scdanieli <23150094+scdanieli@users.noreply.github.com>
2023-10-28 09:36:14 +0200

    chore: use pyproject.toml instead of setup.py

--- setup.py
+++ setup.py
@@ -1,19 +1,6 @@
-from setuptools import setup, find_packages
+# TODO: Remove this file when v15.0.0 is released
+from setuptools import setup
 
-with open("requirements.txt") as f:
-	install_requires = f.read().strip().split("\n")
+name = "ups_integration"
 
-# get version from __version__ variable in ups_integration/__init__.py
-from ups_integration import __version__ as version
-
-setup(
-	name="ups_integration",
-	version=version,
-	description="-",
-	author="ALYF GmbH",
-	author_email="hallo@alyf.de",
-	packages=find_packages(),
-	zip_safe=False,
-	include_package_data=True,
-	install_requires=install_requires
-)
+setup()
